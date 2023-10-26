fcdb03ce6d0ae053275e0c3c02caeb78580f69e0
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2023-10-26 09:00:26 +0200

    improved readability - 73

--- JS_Course/73-date-picker/script.js
+++ JS_Course/73-date-picker/script.js
@@ -16,9 +16,7 @@ import {
 const toggleModalBtn = document.querySelector(".date-picker-button");
 const activeMonthText = document.querySelector(".current-month");
 const datepicker = document.querySelector(".date-picker");
-const datePickerGridDateContainer = document.querySelector(
-	".date-picker-grid-dates"
-);
+const datePickerGridDateContainer = document.querySelector(".date-picker-grid-dates");
 const previousMonthButton = document.querySelector(".prev-month-button");
 const nextMonthButton = document.querySelector(".next-month-button");
 let currentDate = new Date();
@@ -37,9 +35,59 @@ function setUpDatePicker(selectedDate) {
 function setSelectedDateAsSelected(dates) {
 	dates.forEach((date) => {
 		date.classList.remove("selected");
-		if (isSameDay(fromUnixTime(date.dataset.utcdate), currentSelectedDate))
-			date.classList.add("selected");
+		if (isSameDay(fromUnixTime(date.dataset.utcdate), currentSelectedDate)) date.classList.add("selected");
 	});
+}
+
+function showWeeksProportionalToMonthLength(entireMonthByDays, firstWeekStart, dates) {
+	// Add another row to the calendar, if the month is spread over 6 weeks
+	const thursday = 4;
+	const friday = 5;
+	if (
+		(entireMonthByDays.length > 30 && firstWeekStart > thursday) ||
+		(entireMonthByDays.length > 28 && firstWeekStart > friday)
+	) {
+		for (let i = 35; i < 42; i++) {
+			dates[i].classList.toggle("noshow");
+		}
+
+		return 35;
+	} else if (!dates[35].classList.contains("noshow")) {
+		for (let i = 35; i < 42; i++) {
+			dates[i].classList.toggle("noshow");
+		}
+	}
+	return 28;
+}
+
+function setBeforeMonthDays(selectedDate, dates, firstWeekStart) {
+	const lastDayOfLastMonth = endOfMonth(subMonths(selectedDate, 1));
+	for (let weekDay = 0; weekDay < 7 - (7 - firstWeekStart); weekDay++) {
+		const date = dates[weekDay];
+		date.classList.add("date-picker-other-month-date");
+		date.textContent = `${format(subDays(lastDayOfLastMonth, 6 - (7 - firstWeekStart) - weekDay), "d")}`;
+		date.dataset.utcdate = getUnixTime(subDays(lastDayOfLastMonth, 6 - (7 - firstWeekStart) - weekDay));
+	}
+}
+
+function setRegularDays(dates, firstWeekStart, entireMonthByDays, selectedDate) {
+	for (let week = 0; week < 34; week += 7) {
+		for (let weekDay = 0; weekDay <= 7 && week + weekDay < entireMonthByDays.length; weekDay++) {
+			dates[weekDay + firstWeekStart + week].textContent = `${week + weekDay + 1}`;
+			dates[weekDay + firstWeekStart + week].dataset.utcdate = getUnixTime(
+				addDays(startOfMonth(selectedDate), week + weekDay)
+			);
+		}
+	}
+}
+
+function setAfterMonthDays(dates, entireMonthByDays, weeksInMonthInDays) {
+	for (let weekDay = getDay(entireMonthByDays[entireMonthByDays.length - 1]) + 1, i = 1; weekDay < 7; weekDay++, i++) {
+		const day = dates[weekDay + weeksInMonthInDays];
+		day.classList.add("date-picker-other-month-date");
+		day.textContent = i;
+		day.dataset.utcdate = getUnixTime(addDays(startOfMonth(addMonths(currentDate, 1)), i - 1));
+	}
 }
 
 function setupDates(selectedDate) {
@@ -57,69 +105,15 @@ function setupDates(selectedDate) {
 	};
 
 	const entireMonthByDays = eachDayOfInterval(currentMonthInterval);
-	let weeksInMonthInDays = 28;
+
+	let weeksInMonthInDays = showWeeksProportionalToMonthLength(entireMonthByDays, firstWeekStart, dates);
 	// console.log(entireMonthByDays);
 
-	// Add another row to the calendar, if the month is spread over 6 weeks
-	if (
-		(entireMonthByDays.length > 30 && firstWeekStart > 4) ||
-		(entireMonthByDays.length > 28 && firstWeekStart > 5)
-	) {
-		for (let i = 35; i < 42; i++) {
-			dates[i].classList.toggle("noshow");
-		}
+	setBeforeMonthDays(selectedDate, dates, firstWeekStart);
 
-		weeksInMonthInDays = 35;
-	} else if (!dates[35].classList.contains("noshow")) {
-		for (let i = 35; i < 42; i++) {
-			dates[i].classList.toggle("noshow");
-		}
-	}
+	setRegularDays(dates, firstWeekStart, entireMonthByDays, selectedDate);
 
-	// set before other month days
-	const lastDayOfLastMonth = endOfMonth(subMonths(selectedDate, 1));
-	for (let weekDay = 0; weekDay < 7 - (7 - firstWeekStart); weekDay++) {
-		const date = dates[weekDay];
-		date.classList.add("date-picker-other-month-date");
-		date.textContent = `${format(
-			subDays(lastDayOfLastMonth, 6 - (7 - firstWeekStart) - weekDay),
-			"d"
-		)}`;
-		date.dataset.utcdate = getUnixTime(
-			subDays(lastDayOfLastMonth, 6 - (7 - firstWeekStart) - weekDay)
-		);
-	}
-
-	// set regular days
-	for (let week = 0; week < 34; week += 7) {
-		for (
-			let weekDay = 0;
-			weekDay <= 7 && week + weekDay < entireMonthByDays.length;
-			weekDay++
-		) {
-			dates[weekDay + firstWeekStart + week].textContent = `${
-				week + weekDay + 1
-			}`;
-			dates[weekDay + firstWeekStart + week].dataset.utcdate = getUnixTime(
-				addDays(startOfMonth(selectedDate), week + weekDay)
-			);
-		}
-	}
-
-	//set after month days
-	for (
-		let weekDay = getDay(entireMonthByDays[entireMonthByDays.length - 1]) + 1,
-			i = 1;
-		weekDay < 7;
-		weekDay++, i++
-	) {
-		const day = dates[weekDay + weeksInMonthInDays];
-		day.classList.add("date-picker-other-month-date");
-		day.textContent = i;
-		day.dataset.utcdate = getUnixTime(
-			addDays(startOfMonth(addMonths(currentDate, 1)), i - 1)
-		);
-	}
+	setAfterMonthDays(dates, entireMonthByDays, weeksInMonthInDays);
 
 	setSelectedDateAsSelected(dates);
 }
@@ -145,11 +139,13 @@ setDate(new Date());
 datePickerGridDateContainer.addEventListener("click", (e) => {
 	if (!e.target.matches(".date")) return;
 	if (e.target.classList.contains("date-picker-other-month-date")) return;
+
 	let currentSelectedBtn = document.querySelector(".selected");
-	if (currentSelectedBtn != null)
-		currentSelectedBtn.classList.remove("selected");
+	if (currentSelectedBtn != null) currentSelectedBtn.classList.remove("selected");
+
 	e.target.classList.toggle("selected");
 	const selectedDate = fromUnixTime(e.target.dataset.utcdate);
 	currentSelectedDate = selectedDate;
+
 	setDate(selectedDate);
 });
