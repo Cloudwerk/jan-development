416e8c47cd20166df0ddc1154a82f8d986d0154b
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2023-11-13 13:37:18 +0100

    UM: change marker color on map

--- webseite/js/script.ts
+++ webseite/js/script.ts
@@ -3,7 +3,7 @@ import mapboxgl, { LngLatLike } from "mapbox-gl";
 require("dotenv").config();
 const MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN || "";
 
-console.log("API token: " + MAPBOX_ACCESS_TOKEN);
+// console.log("API token: " + MAPBOX_ACCESS_TOKEN);
 
 const CLOUDWERK_COORDS = new mapboxgl.LngLat(10.3301409, 47.7623819);
 
@@ -18,4 +18,4 @@ const map = new mapboxgl.Map({
 const nav = new mapboxgl.NavigationControl({ showZoom: true, showCompass: false });
 map.addControl(nav, "top-right");
 
-const marker1 = new mapboxgl.Marker().setLngLat(CLOUDWERK_COORDS).addTo(map);
+const marker1 = new mapboxgl.Marker({ color: "#007681" }).setLngLat(CLOUDWERK_COORDS).addTo(map);
