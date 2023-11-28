5e4743591bd14dd7834d1f30624f4650a4706a8d
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2023-11-28 11:31:35 +0100

    jsa 55: display weather for today and next days

--- JS_Adv/55-weather-app/client/script.js
+++ JS_Adv/55-weather-app/client/script.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { fromUnixTime } from "date-fns";
 
 navigator.geolocation.getCurrentPosition(positionSuccess, positionError);
 
@@ -11,11 +12,80 @@ function positionError() {
 }
 
 function getWeather(lat, lon) {
-	try {
-		axios.get(`http://localhost:3001/weather`, { params: { lat, lon } }).then((res) => {
+	axios
+		.get(`http://localhost:3001/weather`, { params: { lat, lon } })
+		.then((res) => {
 			console.log(res.data);
+			renderWeather(res.data);
+		})
+		.catch((e) => {
+			console.error(e);
+			alert("Error getting Weather");
 		});
-	} catch (err) {
-		console.error(err);
+}
+
+function renderWeather({ current, daily, hourly }) {
+	document.body.classList.remove("blurred");
+	renderCurrentWeather(current);
+	renderDailyWeather(daily);
+	renderHourlyWeather(hourly);
+}
+
+function setValue(selector, value) {
+	const element = document.querySelector(selector);
+	element.textContent = value;
+}
+
+function setWeatherIcon(selector, icon) {
+	const element = document.querySelector(selector);
+	element.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+}
+
+function renderCurrentWeather(currentWeatherObject) {
+	setValue("[data-current-temp]", currentWeatherObject.currentTemp);
+	setValue("[date-current-description]", currentWeatherObject.description);
+	setWeatherIcon("[current-weather-icon]", currentWeatherObject.icon);
+	setValue("[data-current-high]", currentWeatherObject.highTemp);
+	setValue("[data-current-wind]", currentWeatherObject.windSpeed);
+	setValue("[data-current-low]", currentWeatherObject.lowTemp);
+
+	setValue("[data-current-precip]", currentWeatherObject.precip);
+}
+
+function renderDailyWeather(dailyWeatherObject) {
+	setValue("[data-current-fl-low]", dailyWeatherObject.today.flLow);
+	setValue("[data-current-fl-high]", dailyWeatherObject.today.flHigh);
+	setDailyCardValues("[data-tomorrow]", dailyWeatherObject.tomorrow);
+	setDailyCardValues("[data-third-day]", dailyWeatherObject.thirdDay);
+	setDailyCardValues("[data-fourth-day]", dailyWeatherObject.fourthDay);
+	setDailyCardValues("[data-fifth-day]", dailyWeatherObject.fifthDay);
+}
+
+function setDailyCardValues(parentQuery, { unixTimestamp, average: averageTemperature, icon }) {
+	const currentDay = fromUnixTime(unixTimestamp);
+	setValue(`${parentQuery} .day-card-date`, getDayString(currentDay));
+	setValue(`${parentQuery} .temperature-display`, averageTemperature);
+	setWeatherIcon(`${parentQuery} .weather-icon`, icon);
+}
+
+function getDayString(date) {
+	const day = date.getDay();
+	switch (day) {
+		case 0:
+			return "Sunday";
+		case 1:
+			return "Monday";
+		case 2:
+			return "Tuesday";
+		case 3:
+			return "Wednesday";
+		case 4:
+			return "Thursday";
+		case 5:
+			return "Friday";
+		case 6:
+			return "Saturday";
 	}
 }
+
+function renderHourlyWeather(hourlyWeatherObject) {}
--- JS_Adv/55-weather-app/server/server.js
+++ JS_Adv/55-weather-app/server/server.js
@@ -1,3 +1,6 @@
+const fromUnixTime = require("date-fns/fromUnixTime");
+const isSameDay = require("date-fns/isSameDay");
+const add = require("date-fns/add");
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
@@ -15,11 +18,17 @@ app.get("/weather", (req, res) => {
 			params: {
 				lat,
 				lon,
+				units: "metric",
 				appid: process.env.API_KEY,
 			},
 		})
 		.then(({ data }) => {
-			res.json(data);
+			res.json({
+				daily: parseDailyWeather(data),
+				current: parseCurrentWeather(data),
+				hourly: parseHourlyWeather(data),
+				data,
+			});
 		})
 		.catch((e) => {
 			console.log(e);
@@ -28,3 +37,77 @@ app.get("/weather", (req, res) => {
 });
 
 app.listen(3001);
+
+function parseDailyWeather({ list }) {
+	const dayOne = fromUnixTime(list[0].dt);
+	const dayTwo = add(dayOne, { days: 1 });
+	const dayThree = add(dayTwo, { days: 1 });
+	const dayFour = add(dayThree, { days: 1 });
+	const dayFive = add(dayFour, { days: 1 });
+	return {
+		today: calculateDailyAverageAndIcon(dayOne, list),
+		tomorrow: calculateDailyAverageAndIcon(dayTwo, list),
+		thirdDay: calculateDailyAverageAndIcon(dayThree, list),
+		fourthDay: calculateDailyAverageAndIcon(dayFour, list),
+		fifthDay: calculateDailyAverageAndIcon(dayFive, list),
+	};
+}
+
+function calculateDailyAverageAndIcon(day, list) {
+	const sameDayWeather = list.filter((entry) => isSameDay(fromUnixTime(entry.dt), day));
+	if (sameDayWeather.length > 0)
+		return {
+			average: Math.round(
+				sameDayWeather.reduce(function (avg, value, _, { length }) {
+					return avg + value.main.temp / length;
+				}, 0)
+			),
+			flHigh: Math.round(Math.max(...sameDayWeather.map((e) => e.main.feels_like))),
+			flLow: Math.round(Math.min(...sameDayWeather.map((e) => e.main.feels_like))),
+			unixTimestamp: sameDayWeather[0].dt,
+			icon: sameDayWeather[0].weather[0].icon,
+		};
+	else return null;
+}
+
+function parseCurrentWeather({ list }) {
+	const { main, weather, wind, pop } = list[0];
+	return {
+		currentTemp: Math.round(main.temp),
+		highTemp: Math.round(main.temp_max),
+		lowTemp: Math.round(main.temp_min),
+		highFeelsLike: "missing",
+		lowFeelslike: "missing",
+		windSpeed: Math.round(wind.speed),
+		precip: Math.round(pop * 100),
+		icon: weather[0].icon,
+		description: weather[0].description,
+	};
+}
+
+function parseHourlyWeather({ list }) {
+	const nextEightStamps = list.slice(0, 8);
+	// const returnValues = [];
+	// nextEightStamps.forEach((element) => {
+	// 	returnValues.push({
+	// 		timestamp: element.dt,
+	// 		icon: element.weather[0].icon,
+	// 		temp: element.main.temp,
+	// 		tempFeelsLike: element.main.feels_like,
+	// 		wind: element.wind.speed,
+	// 		precip: element.pop,
+	// 	});
+	// });
+	// return returnValues;
+
+	return nextEightStamps.map((element) => {
+		return {
+			timestamp: element.dt,
+			icon: element.weather[0].icon,
+			temp: Math.round(element.main.temp),
+			tempFeelsLike: Math.round(element.main.feels_like),
+			wind: Math.round(element.wind.speed),
+			precip: element.pop,
+		};
+	});
+}
