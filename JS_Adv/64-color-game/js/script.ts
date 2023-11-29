42bf87bdf080590213f89125c248c2996fbcb934
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2023-11-29 10:38:12 +0100

    jsa 64: change color display on select

--- JS_Adv/64-color-game/js/script.ts
+++ JS_Adv/64-color-game/js/script.ts
@@ -3,6 +3,7 @@ import { COLOR_FORMATS, DIFFICULTIES } from "./color-game.ts";
 const resultsElement = document.querySelector(".results");
 const colorElement = document.querySelector(".form #color-selector");
 const difficultyElement = document.querySelector(".form #difficulty-selector");
+const colorDisplay = document.querySelector(".color-string");
 
 let colorFormat = COLOR_FORMATS.RGB;
 let difficulty = DIFFICULTIES.EASY;
@@ -15,14 +16,18 @@ function onStart() {
 }
 
 function setupEventListeners() {
+	if (colorDisplay == null) return;
 	colorElement?.addEventListener("change", (e) => {
 		const target = <HTMLInputElement>e.target;
 		colorFormat = target.id;
+		colorDisplay.textContent = colorFormat;
+		//TODO regenerate colors
 		console.log(colorFormat);
 	});
 	difficultyElement?.addEventListener("change", (e) => {
 		const target = <HTMLInputElement>e.target;
 		difficulty = target.id;
+		//TODO regenerate colors
 		console.log(difficulty);
 	});
 }
