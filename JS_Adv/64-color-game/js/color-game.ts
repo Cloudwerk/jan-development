932aa60152e1f4387c0e54d0224ab6684aa3b44d
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2023-11-29 10:25:56 +0100

    jsa 64: events listeners for the radio buttons

---
+++ JS_Adv/64-color-game/js/color-game.ts
@@ -0,0 +1,10 @@
+export const COLOR_FORMATS = {
+	RGB: "rgb",
+	HEX: "hex",
+	HSL: "hsl",
+};
+export const DIFFICULTIES = {
+	EASY: "easy",
+	MEDIUM: "medium",
+	HARD: "hard",
+};
--- JS_Adv/64-color-game/js/script.ts
+++ JS_Adv/64-color-game/js/script.ts
@@ -1,6 +1,31 @@
-const resultsElement = document.querySelector(".results");
+import { COLOR_FORMATS, DIFFICULTIES } from "./color-game.ts";
 
-toggleResult();
+const resultsElement = document.querySelector(".results");
+const colorElement = document.querySelector(".form #color-selector");
+const difficultyElement = document.querySelector(".form #difficulty-selector");
+
+let colorFormat = COLOR_FORMATS.RGB;
+let difficulty = DIFFICULTIES.EASY;
+
+onStart();
+
+function onStart() {
+	toggleResult();
+	setupEventListeners();
+}
+
+function setupEventListeners() {
+	colorElement?.addEventListener("change", (e) => {
+		const target = <HTMLInputElement>e.target;
+		colorFormat = target.id;
+		console.log(colorFormat);
+	});
+	difficultyElement?.addEventListener("change", (e) => {
+		const target = <HTMLInputElement>e.target;
+		difficulty = target.id;
+		console.log(difficulty);
+	});
+}
 
 function toggleResult() {
 	resultsElement?.classList.toggle("hide");
