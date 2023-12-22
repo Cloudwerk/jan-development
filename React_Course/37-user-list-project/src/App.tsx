8476d72c678260cdb2ed501c65f5b778c61bef97
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2023-12-22 15:21:19 +0100

    react 37: added fetch request

--- React_Course/37-user-list-project/src/App.tsx
+++ React_Course/37-user-list-project/src/App.tsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-	return <h1>Hello World!</h1>;
+	const [users, setUsers] = useState();
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState();
+
+	useEffect(() => {
+		setLoading(true);
+		setError(undefined);
+		const controller = new AbortController();
+		fetch("https://jsonplaceholder.typicode.com/users", { signal: controller.signal })
+			.then((res) => {
+				if (res.status == 200) return res.json();
+				else return Promise.reject(res);
+			})
+			.then((data) => {
+				setUsers(data);
+			})
+			.catch((e) => {
+				if (e?.name === "AbortError") return;
+				setError(e);
+			})
+			.finally(() => setLoading(false));
+
+		return () => {
+			controller.abort();
+		};
+	}, []);
+
+	return (
+		<>
+			<h1>User List</h1>
+			<ul>{JSON.stringify(users)}</ul>
+		</>
+	);
 }
 
 export default App;
