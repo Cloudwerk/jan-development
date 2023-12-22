34016f5facea645611c8c202838bf7c8a4e868cc
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2023-12-22 13:03:24 +0100

    react 27

---
+++ React_Course/27-use-effect-exercises/src/Child.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+
+export function Child() {
+	const [age, setAge] = useState(0);
+	const [name, setName] = useState("");
+	console.log("Render");
+
+	useEffect(() => {
+		console.log("Hi");
+
+		return () => {
+			console.log("Bye");
+		};
+	});
+
+	useEffect(() => {
+		console.log(`My name is ${name} and I am ${age} years old`);
+	}, [name, age]);
+
+	useEffect(() => {
+		document.title = name;
+	}, [name]);
+
+	useEffect(() => {
+		document.title = name;
+
+		const timeout = setTimeout(() => {
+			console.log(`My name is ${name}`);
+		}, 1000);
+
+		return () => {
+			clearTimeout(timeout);
+		};
+	}, [name]);
+
+	return (
+		<div>
+			<input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+			<br />
+			<br />
+			<button onClick={() => setAge((a) => a - 1)}>-</button>
+			{age}
+			<button onClick={() => setAge((a) => a + 1)}>+</button>
+			<br />
+			<br />
+			My name is {name} and I am {age} years old.
+		</div>
+	);
+}
