97dbe348a6c783dd6bc6af368f238cb74a36621c
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-01-02 11:13:58 +0100

    react 41: add form

--- React_Course/41-todo-list/src/App.tsx
+++ React_Course/41-todo-list/src/App.tsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+import "./styles.css";
+
+function AddNewListItem() {}
+
 function App() {
+	const [items, setItems] = useState(Array<string>);
+	let newItemName = "";
+	// console.log(items);
+
 	return (
 		<>
-			<div>Hello, World!</div>
+			<div id="new-todo-form">
+				<label htmlFor="todo-input">New Todo</label>
+				<input
+					type="text"
+					id="todo-input"
+					onChange={(evt) => {
+						newItemName = evt.target.value;
+					}}
+				/>
+				<button
+					onClick={() => {
+						if (newItemName == "") return;
+						setItems([...items, newItemName]);
+					}}
+				>
+					Add Todo
+				</button>
+			</div>
 		</>
 	);
 }
