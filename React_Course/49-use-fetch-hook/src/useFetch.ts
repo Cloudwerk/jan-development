99ddc49a355a28efdde982ba8d8d0dad6402411c
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-01-02 15:39:10 +0100

    react 49: finish

--- React_Course/49-use-fetch-hook/src/useFetch.ts
+++ React_Course/49-use-fetch-hook/src/useFetch.ts
@@ -1,4 +1,6 @@
-interface IOptionsObject {
+import { useEffect, useState } from "react";
+
+interface IUseFetchOptions {
 	method: string;
 	body: string;
 	headers: {
@@ -6,6 +8,32 @@ interface IOptionsObject {
 	};
 }
 
-export function useFetch(url: string) {
-	return { data: "", isLoading: "", isError: "" };
+export function useFetch(_url: string) {
+	const [data, setData] = useState();
+	const [isLoading, setIsLoading] = useState(true);
+	const [isError, setIsError] = useState(false);
+
+	useEffect(() => {
+		setData(undefined);
+		setIsError(false);
+		setIsLoading(true);
+
+		const controller = new AbortController();
+		fetch(_url, { signal: controller.signal })
+			.then((res) => res.json())
+			.then(setData)
+			.catch((e) => {
+				if (e.name === "AbortError") return;
+				setIsError(true);
+			})
+			.finally(() => {
+				if (controller.signal.aborted) return;
+				setIsLoading(false);
+			});
+
+		return () => {
+			controller.abort();
+		};
+	}, [_url]);
+	return { data, isLoading, isError };
 }
