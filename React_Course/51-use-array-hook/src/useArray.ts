b278e8ba067b0b79976ae94c394c36805aa3cc18
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-01-02 16:22:36 +0100

    react 51: filter

--- React_Course/51-use-array-hook/src/useArray.ts
+++ React_Course/51-use-array-hook/src/useArray.ts
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useState } from "react";
 
 export function useArray(initialArray: Array<number>) {
 	const [_array, setArray] = useState(initialArray);
@@ -9,12 +9,16 @@ export function useArray(initialArray: Array<number>) {
 			setArray([..._array.slice(0, indexToReplace), replacement, ..._array.slice(indexToReplace + 1)]),
 		[_array]
 	);
+	const filter = useCallback(
+		(callback: (value: number, index?: number, array?: number[]) => boolean) => setArray(_array.filter(callback)),
+		[_array]
+	);
 
 	function set(array: Array<number>) {
 		setArray(array);
 	}
 	// function replace(toReplace: number, replacement: number) {}
-	function filter(callback: CallableFunction) {}
+	// function filter(callback: CallableFunction) {}
 	function remove(index: number) {}
 	function clear() {}
 	function reset() {}
