b80f25b737872168977de239d56e151983a5bea5
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-01-02 16:10:28 +0100

    react 51: replace

--- React_Course/51-use-array-hook/src/useArray.ts
+++ React_Course/51-use-array-hook/src/useArray.ts
@@ -4,11 +4,16 @@ export function useArray(initialArray: Array<number>) {
 	const [_array, setArray] = useState(initialArray);
 
 	const push = useCallback((number: number) => setArray([..._array, number]), [_array]);
+	const replace = useCallback(
+		(indexToReplace: number, replacement: number) =>
+			setArray([..._array.slice(0, indexToReplace), replacement, ..._array.slice(indexToReplace + 1)]),
+		[_array]
+	);
 
 	function set(array: Array<number>) {
 		setArray(array);
 	}
-	function replace(toReplace: number, replacement: number) {}
+	// function replace(toReplace: number, replacement: number) {}
 	function filter(callback: CallableFunction) {}
 	function remove(index: number) {}
 	function clear() {}
