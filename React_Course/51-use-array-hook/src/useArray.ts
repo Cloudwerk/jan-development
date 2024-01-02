f80a7e58f5bb2be43a56c9aeda6a092a17468f74
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-01-02 16:07:06 +0100

    react 51: push

--- React_Course/51-use-array-hook/src/useArray.ts
+++ React_Course/51-use-array-hook/src/useArray.ts
@@ -3,14 +3,15 @@ import { useCallback, useMemo, useState } from "react";
 export function useArray(initialArray: Array<number>) {
 	const [_array, setArray] = useState(initialArray);
 
+	const push = useCallback((number: number) => setArray([..._array, number]), [_array]);
+
 	function set(array: Array<number>) {
 		setArray(array);
 	}
-	function push(number: number) {}
 	function replace(toReplace: number, replacement: number) {}
 	function filter(callback: CallableFunction) {}
 	function remove(index: number) {}
 	function clear() {}
 	function reset() {}
-	return { array: [], set, push, replace, filter, remove, clear, reset };
+	return { array: _array, set, push, replace, filter, remove, clear, reset };
 }
