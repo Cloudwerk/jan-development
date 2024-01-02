6b1e6bd2b7b27ec0b5078a37637ef7d03967bb6b
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-01-02 15:04:46 +0100

    react 49: create interface for options

--- React_Course/49-use-fetch-hook/src/useFetch.ts
+++ React_Course/49-use-fetch-hook/src/useFetch.ts
@@ -1,3 +1,11 @@
+interface IOptionsObject {
+	method: string;
+	body: string;
+	headers: {
+		"Content-type": string;
+	};
+}
+
 export function useFetch(url: string) {
 	return { data: "", isLoading: "", isError: "" };
 }
