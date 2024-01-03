a5ea9c697614a8fb0fbdb990d5fa0dfad064eab5
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-01-03 14:04:33 +0100

    react 58: ref version

---
+++ React_Course/58-basic-form-validation/src/AppRef.tsx
@@ -0,0 +1,65 @@
+import { useState, useRef } from "react";
+import "./styles.css";
+import { ErrorMessage } from "./ErrorMessage";
+import { emailValidation, passwordValidation } from "./validators";
+
+function AppRef() {
+	const mailRef = useRef<any>();
+	const passwordRef = useRef<any>();
+	const [passwordErrorMessages, setPasswordErrorMessages] = useState(Array<string>);
+	const [mailErrorMessage, setMailErrorMessage] = useState("");
+
+	const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
+
+	function onFormSubmit(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		setHasBeenSubmitted(true);
+
+		setMailErrorMessage(emailValidation(mailRef.current.value));
+		setPasswordErrorMessages(passwordValidation(passwordRef.current.value));
+	}
+
+	return (
+		<>
+			<form className="form" onSubmit={(e) => onFormSubmit(e)}>
+				<div className={`form-group ${mailErrorMessage !== "" ? "error" : ""}`}>
+					<label className="label" htmlFor="email">
+						Email
+					</label>
+					<input
+						className="input"
+						type="email"
+						id="email"
+						ref={mailRef}
+						onChange={() => {
+							hasBeenSubmitted ? setMailErrorMessage(emailValidation(mailRef.current.value)) : [];
+						}}
+					/>
+					{mailErrorMessage !== "" ? <ErrorMessage message={mailErrorMessage}></ErrorMessage> : <></>}
+				</div>
+				<div className={`form-group ${passwordErrorMessages.length >= 1 ? "error" : ""}`}>
+					<label className="label" htmlFor="password">
+						Password
+					</label>
+					<input
+						className="input"
+						ref={passwordRef}
+						type="password"
+						id="password"
+						onChange={() => {
+							hasBeenSubmitted ? setPasswordErrorMessages(passwordValidation(passwordRef.current.value)) : [];
+						}}
+					/>
+					{passwordErrorMessages.map((msg) => {
+						return <ErrorMessage message={msg}></ErrorMessage>;
+					})}
+				</div>
+				<button className="btn" type="submit">
+					Submit
+				</button>
+			</form>
+		</>
+	);
+}
+
+export default AppRef;
---
+++ React_Course/58-basic-form-validation/src/validators.ts
@@ -0,0 +1,36 @@
+const PW_LC_REGEX = /([a-z])+/;
+const PW_UC_REGEX = /([A-Z])+/;
+const NUMBER_REGEX = /([0-9])+/;
+
+export function emailValidation(mail: string) {
+	let mailErrorMessage = "";
+	if (mail === "") {
+		mailErrorMessage = "Cannot be blank!";
+	} else if (!mail.match("@webdevsimplified.com")) {
+		mailErrorMessage = "Must end in @webdevsimplified.com";
+	}
+
+	return mailErrorMessage;
+}
+
+export function passwordValidation(password: string) {
+	const errorMessages = [];
+
+	if (password === "") {
+		errorMessages.push("Cannot be blank!");
+	}
+	if (password.length < 10) {
+		errorMessages.push("Must be 10 characters or longer!");
+	}
+	if (!password.match(PW_LC_REGEX)) {
+		errorMessages.push("Must include a lowercase letter");
+	}
+	if (!password.match(PW_UC_REGEX)) {
+		errorMessages.push("Must include a uppercase letter");
+	}
+	if (!password.match(NUMBER_REGEX)) {
+		errorMessages.push("Must include a number");
+	}
+
+	return errorMessages;
+}
