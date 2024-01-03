5ddd291d0cbc97d9bd3c5562ee497a3a3a746d61
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-01-03 10:55:58 +0100

    react 58: implement mail validation and error

--- React_Course/58-basic-form-validation/src/App.tsx
+++ React_Course/58-basic-form-validation/src/App.tsx
@@ -4,23 +4,36 @@ import "./styles.css";
 function App() {
 	const [mail, setMail] = useState("test@test.com");
 	const [password, setPassword] = useState("");
-	const [passwordError, setPasswordError] = useState("");
-	let mailError = false;
+	const [passwordError, setPasswordError] = useState(false);
+	const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
+	const [isMailError, setIsMailError] = useState(false);
+	const [mailErrorMessage, setMailErrorMessage] = useState("");
 
 	function onFormSubmit(e: React.FormEvent<HTMLFormElement>) {
 		e.preventDefault();
+
+		if (mail === "") {
+			setIsMailError(true);
+			setMailErrorMessage("Cannot be blank!");
+		} else if (!mail.match("@webdevsimplified.com")) {
+			setIsMailError(true);
+			setMailErrorMessage("Must end in @webdevsimplified.com");
+		} else if (isMailError) {
+			setIsMailError(false);
+		}
+
 		//validation
 	}
 
 	return (
 		<>
 			<form className="form" onSubmit={(e) => onFormSubmit(e)}>
-				<div className={`form-group ${mailError ? "error" : ""}`}>
+				<div className={`form-group ${isMailError ? "error" : ""}`}>
 					<label className="label" htmlFor="email">
 						Email
 					</label>
 					<input className="input" type="email" id="email" value={mail} onChange={(e) => setMail(e.target.value)} />
-					<div className={`msg ${mailError ? "" : "hidden"}`}>Must end in @webdevsimplified.com</div>
+					<div className={`msg ${isMailError ? "" : "hidden"}`}>{mailErrorMessage}</div>
 				</div>
 				<div className="form-group">
 					<label className="label" htmlFor="password">
