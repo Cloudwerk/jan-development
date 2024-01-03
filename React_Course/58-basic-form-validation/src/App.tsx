a5dc6f05df84378d1e06bc92eef919fce8420008
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-01-03 10:44:21 +0100

    react 58: setup error display mechanism

--- React_Course/58-basic-form-validation/src/App.tsx
+++ React_Course/58-basic-form-validation/src/App.tsx
@@ -1,21 +1,38 @@
+import { useState } from "react";
 import "./styles.css";
 
 function App() {
+	const [mail, setMail] = useState("test@test.com");
+	const [password, setPassword] = useState("");
+	const [passwordError, setPasswordError] = useState("");
+	let mailError = false;
+
+	function onFormSubmit(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		//validation
+	}
+
 	return (
 		<>
-			<form className="form">
-				<div className="form-group error">
+			<form className="form" onSubmit={(e) => onFormSubmit(e)}>
+				<div className={`form-group ${mailError ? "error" : ""}`}>
 					<label className="label" htmlFor="email">
 						Email
 					</label>
-					<input className="input" type="email" id="email" value="test@test.com" />
-					<div className="msg">Must end in @webdevsimplified.com</div>
+					<input className="input" type="email" id="email" value={mail} onChange={(e) => setMail(e.target.value)} />
+					<div className={`msg ${mailError ? "" : "hidden"}`}>Must end in @webdevsimplified.com</div>
 				</div>
 				<div className="form-group">
 					<label className="label" htmlFor="password">
 						Password
 					</label>
-					<input className="input" value="Password123!" type="password" id="password" />
+					<input
+						className="input"
+						value={password}
+						type="password"
+						id="password"
+						onChange={(e) => setPassword(e.target.value)}
+					/>
 				</div>
 				<button className="btn" type="submit">
 					Submit
