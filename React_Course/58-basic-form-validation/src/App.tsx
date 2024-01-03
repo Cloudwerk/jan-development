2f9725fa5659ba22b0e79549b306d76a922c54ac
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-01-03 10:21:11 +0100

    react 58: add starting code

--- React_Course/58-basic-form-validation/src/App.tsx
+++ React_Course/58-basic-form-validation/src/App.tsx
@@ -1,7 +1,28 @@
 import "./styles.css";
 
 function App() {
-	return <h1>Hello, World!</h1>;
+	return (
+		<>
+			<form className="form">
+				<div className="form-group error">
+					<label className="label" htmlFor="email">
+						Email
+					</label>
+					<input className="input" type="email" id="email" value="test@test.com" />
+					<div className="msg">Must end in @webdevsimplified.com</div>
+				</div>
+				<div className="form-group">
+					<label className="label" htmlFor="password">
+						Password
+					</label>
+					<input className="input" value="Password123!" type="password" id="password" />
+				</div>
+				<button className="btn" type="submit">
+					Submit
+				</button>
+			</form>
+		</>
+	);
 }
 
 export default App;
