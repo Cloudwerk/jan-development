2850076d9d47c7695e7b6bce2f994a510d9d0a6c
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-01-09 10:15:17 +0100

    react 75: fetch users Posts for User Page

--- React_Course/75-basic-router/src/pages/User.tsx
+++ React_Course/75-basic-router/src/pages/User.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useFetch } from "../utils/useFetch";
-import { IUserFetchReturn } from "../utils/types";
+import { IUseFetchPostsReturn, IUserFetchReturn } from "../utils/types";
+import { PostCard } from "./components/PostCard";
 
 const API_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -16,6 +17,11 @@ export function User() {
 	if (userID == null) return <h1>Invalid User ID!</h1>;
 
 	const { data: fetchData, isError, isLoading }: IUserFetchReturn = useFetch(`${API_URL}/users/${userID}`);
+	const {
+		data: postsData,
+		isError: postsIsError,
+		isLoading: postsIsLoading,
+	}: IUseFetchPostsReturn = useFetch(`${API_URL}/posts?userId=${userID}`);
 
 	return (
 		<div className="container">
@@ -43,90 +49,13 @@ export function User() {
 			)}
 			<h3 className="mt-4 mb-2">Posts</h3>
 			<div className="card-grid">
-				<div className="card">
-					<div className="card-header">sunt aut facere repellat provident occaecati excepturi optio reprehenderit</div>
-					<div className="card-body">
-						<div className="card-preview-text">
-							quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam
-							nostrum rerum est autem sunt rem eveniet architecto
-						</div>
-					</div>
-					<div className="card-footer">
-						<a className="btn" href="posts.html">
-							View
-						</a>
-					</div>
-				</div>
-				<div className="card">
-					<div className="card-header">qui est esse</div>
-					<div className="card-body">
-						<div className="card-preview-text">
-							est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis
-							voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla
-						</div>
-					</div>
-					<div className="card-footer">
-						<a className="btn" href="posts.html">
-							View
-						</a>
-					</div>
-				</div>
-				<div className="card">
-					<div className="card-header">ea molestias quasi exercitationem repellat qui ipsa sit aut</div>
-					<div className="card-body">
-						<div className="card-preview-text">
-							et iusto sed quo iure voluptatem occaecati omnis eligendi aut ad voluptatem doloribus vel accusantium quis
-							pariatur molestiae porro eius odio et labore et velit aut
-						</div>
-					</div>
-					<div className="card-footer">
-						<a className="btn" href="posts.html">
-							View
-						</a>
-					</div>
-				</div>
-				<div className="card">
-					<div className="card-header">eum et est occaecati</div>
-					<div className="card-body">
-						<div className="card-preview-text">
-							ullam et saepe reiciendis voluptatem adipisci sit amet autem assumenda provident rerum culpa quis hic
-							commodi nesciunt rem tenetur doloremque ipsam iure quis sunt voluptatem rerum illo velit
-						</div>
-					</div>
-					<div className="card-footer">
-						<a className="btn" href="posts.html">
-							View
-						</a>
-					</div>
-				</div>
-				<div className="card">
-					<div className="card-header">nesciunt quas odio</div>
-					<div className="card-body">
-						<div className="card-preview-text">
-							repudiandae veniam quaerat sunt sed alias aut fugiat sit autem sed est voluptatem omnis possimus esse
-							voluptatibus quis est aut tenetur dolor neque
-						</div>
-					</div>
-					<div className="card-footer">
-						<a className="btn" href="posts.html">
-							View
-						</a>
-					</div>
-				</div>
-				<div className="card">
-					<div className="card-header">dolorem eum magni eos aperiam quia</div>
-					<div className="card-body">
-						<div className="card-preview-text">
-							ut aspernatur corporis harum nihil quis provident sequi mollitia nobis aliquid molestiae perspiciatis et
-							ea nemo ab reprehenderit accusantium quas voluptate dolores velit et doloremque molestiae
-						</div>
-					</div>
-					<div className="card-footer">
-						<a className="btn" href="posts.html">
-							View
-						</a>
-					</div>
-				</div>
+				{postsIsLoading
+					? "Loading..."
+					: postsIsError
+					? "There has been an Error"
+					: postsData!.map((post) => {
+							return <PostCard {...post} />;
+					  })}
 			</div>
 			<h3 className="mt-4 mb-2">Todos</h3>
 			<ul>
