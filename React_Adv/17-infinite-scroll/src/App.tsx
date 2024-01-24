334207a2b06075316c489b760106ac34e1fa5026
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-01-24 12:15:55 +0100

    React-ADv: 17 improve img insertion

--- React_Adv/17-infinite-scroll/src/App.tsx
+++ React_Adv/17-infinite-scroll/src/App.tsx
@@ -1,20 +1,45 @@
+import { useEffect, useState } from "react";
 import { IImageData, IUseFetchLinkHeaderValues, useFetchWithLinkHeader } from "./utils/useFetch";
 
 const URL_SHORTLIST = "http://127.0.0.1:3000/photos-short-list";
 
 function App() {
+	const [currentPage, setCurrentPage] = useState(1);
+	const [images, setImages] = useState(Array<IImageData>);
 	const {
 		loading,
 		error,
 		value: imgData,
-	} = useFetchWithLinkHeader<IUseFetchLinkHeaderValues<IImageData>>(`${URL_SHORTLIST}?_page=1&_limit=10`, {
+	} = useFetchWithLinkHeader<IUseFetchLinkHeaderValues<IImageData>>(`${URL_SHORTLIST}?_page=${currentPage}&_limit=20`, {
 		method: "GET",
 	});
 
+	// function getNewImages() {
+	// 	const {
+	// 		loading,
+	// 		error,
+	// 		value: _imgData,
+	// 	} = useFetchWithLinkHeader<IUseFetchLinkHeaderValues<IImageData>>(
+	// 		`${URL_SHORTLIST}?_page=${currentPage}&_limit=20`,
+	// 		{
+	// 			method: "GET",
+	// 		}
+	// 	);
+	// 	setCurrentPage((page) => page + 1)
+	//   return _imgData;
+	// }
+
+	useEffect(() => {
+		if (imgData)
+			setImages((oldImgs) => {
+				return [...oldImgs, ...imgData.data];
+			});
+	}, [imgData]);
+
 	return (
 		<div className="grid">
-			{imgData?.data.map((img) => {
-				return <img src={img.url}></img>;
+			{images.map((img, index) => {
+				return <img key={index} src={img.url} />;
 			})}
 			<img src="https://via.placeholder.com/600/92c952" />
 			<img src="https://via.placeholder.com/600/771796" />
