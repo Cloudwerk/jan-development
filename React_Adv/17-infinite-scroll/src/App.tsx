880b1c4c88eac9eb572558e5190c3070e8b90dc4
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-01-24 10:59:19 +0100

    React-Adv. 17: add starting files

---
+++ React_Adv/17-infinite-scroll/src/App.tsx
@@ -0,0 +1,20 @@
+function App() {
+	return (
+		<div className="grid">
+			<img src="https://via.placeholder.com/600/92c952" />
+			<img src="https://via.placeholder.com/600/771796" />
+			<img src="https://via.placeholder.com/600/24f355" />
+			<img src="https://via.placeholder.com/600/d32776" />
+			<img src="https://via.placeholder.com/600/f66b97" />
+			<img src="https://via.placeholder.com/600/92c952" />
+			<div className="skeleton">Loading...</div>
+			<div className="skeleton">Loading...</div>
+			<div className="skeleton">Loading...</div>
+			<div className="skeleton">Loading...</div>
+			<div className="skeleton">Loading...</div>
+			<div className="skeleton">Loading...</div>
+		</div>
+	);
+}
+
+export default App;
