81a53965696b82d1a1dfa7c1a210299797d66083
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-01-26 08:21:52 +0100

    React Adv. 17: Add Loading animation

--- React_Adv/17-infinite-scroll/src/App.tsx
+++ React_Adv/17-infinite-scroll/src/App.tsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from "react";
 import { IImageData, IUseFetchLinkHeaderValues, useFetchWithLinkHeader } from "./utils/useFetch";
 
 const URL_SHORTLIST = "http://127.0.0.1:3000/photos";
+const LIMIT = 20;
 
 function App() {
 	const [currentPage, setCurrentPage] = useState(1);
@@ -11,7 +12,7 @@ function App() {
 		error,
 		value: imgData,
 	} = useFetchWithLinkHeader<IUseFetchLinkHeaderValues<IImageData>>(
-		`${URL_SHORTLIST}?_page=${currentPage}&_limit=20`,
+		`${URL_SHORTLIST}?_page=${currentPage}&_limit=${LIMIT.toString()}`,
 		{
 			method: "GET",
 		},
@@ -43,6 +44,17 @@ function App() {
 				if (index === images.length - 1) return <img key={index} src={img.url} ref={lastImgRef} />;
 				return <img key={index} src={img.url} />;
 			})}
+			{loading
+				? Array(LIMIT)
+						.fill(null)
+						.map((_, index) => {
+							return (
+								<div className="skeleton" key={`skeleton-${index}`}>
+									Loading...
+								</div>
+							);
+						})
+				: ""}
 		</div>
 	);
 }
