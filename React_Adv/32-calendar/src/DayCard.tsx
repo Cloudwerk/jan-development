84280b9c56cc12ee25dad55fc1812e3586730b71
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-01-29 09:59:32 +0100

    React-Adv 32: Properly handle Days Cards from dates outside the current month

--- React_Adv/32-calendar/src/DayCard.tsx
+++ React_Adv/32-calendar/src/DayCard.tsx
@@ -6,11 +6,37 @@ interface IDayCardsProps {
 }
 
 export function DayCard({ day, currentMonth }: IDayCardsProps) {
-	const sameDay = isSameDay(day, currentMonth);
+	const sameDay = isSameDay(day, Date.now());
+	if (isSameMonth(currentMonth, new Date())) {
+		return (
+			<div
+				className={`day ${isSameMonth(day, currentMonth) ? "" : "non-month-day"} ${
+					isBefore(day, currentMonth) && !sameDay ? "old-month-day" : ""
+				}`}
+			>
+				<div className="day-header">
+					<div className="week-name">{format(day, "eee")}</div>
+					<div className={`day-number ${sameDay ? "today" : ""}`}>{format(day, "d")}</div>
+					<button className="add-event-btn">+</button>
+				</div>
+			</div>
+		);
+	}
+	if (isBefore(currentMonth, new Date())) {
+		return (
+			<div className={`day ${isSameMonth(day, currentMonth) ? "" : "non-month-day"} old-month-day`}>
+				<div className="day-header">
+					<div className="week-name">{format(day, "eee")}</div>
+					<div className={`day-number`}>{format(day, "d")}</div>
+					<button className="add-event-btn">+</button>
+				</div>
+			</div>
+		);
+	}
 	return (
 		<div
 			className={`day ${isSameMonth(day, currentMonth) ? "" : "non-month-day"} ${
-				isBefore(day, currentMonth) && !sameDay ? "old-month-day" : ""
+				isBefore(day, Date.now()) && !sameDay ? "old-month-day" : ""
 			}`}
 		>
 			<div className="day-header">
