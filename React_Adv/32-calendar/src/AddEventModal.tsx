eeed8fe057b8cf3e09ef1a4a19d58caf37bd6c84
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-01-31 08:32:31 +0100

    React-Adv 32: fix setting data in Local Storage

--- React_Adv/32-calendar/src/AddEventModal.tsx
+++ React_Adv/32-calendar/src/AddEventModal.tsx
@@ -24,7 +24,7 @@ export function AddEventModal({ day, setAddEventModalDate, addEventModalDate }: 
 	const startTimeRef = useRef<HTMLInputElement>(null);
 	const endTimeRef = useRef<HTMLInputElement>(null);
 	const [color, setColor] = useState<"Blue" | "Green" | "Red">("Blue");
-	const { value, setValue } = useLocalStorage(`${format(day, "yyyy_MM_dd")}-event`, Array<IEventProps>());
+	const { setValue } = useLocalStorage(`${format(day, "yyyy_MM_dd")}-event`, Array<IEventProps>());
 
 	function clearForm() {
 		nameRef.current ? (nameRef.current.value = "") : "";
@@ -40,13 +40,16 @@ export function AddEventModal({ day, setAddEventModalDate, addEventModalDate }: 
 
 	function formSubmit(e: React.FormEvent<HTMLFormElement>) {
 		e.preventDefault();
-		setValue({
-			name: nameRef.current?.value,
-			allDayRef: allDayRef.current?.checked,
-			startTime: startTimeRef.current?.valueAsDate,
-			endTime: endTimeRef.current?.valueAsDate,
-			color: color,
-		});
+		setValue((oldData: Array<IEventProps>) => [
+			...oldData,
+			{
+				name: nameRef.current?.value,
+				allDayRef: allDayRef.current?.checked,
+				startTime: startTimeRef.current?.valueAsDate,
+				endTime: endTimeRef.current?.valueAsDate,
+				color: color,
+			},
+		]);
 	}
 
 	return createPortal(
