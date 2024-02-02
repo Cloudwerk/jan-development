28b6a29fabccf073d2ae6b138a21637207067919
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-02-02 10:47:20 +0100

    React-Adv. 41: Add starting Files

---
+++ React_Adv/42-component-testing/setupTests.js
@@ -0,0 +1,9 @@
+import matchers from "@testing-library/jest-dom/matchers";
+import { expect, afterEach } from "vitest";
+import { cleanup } from "@testing-library/react";
+
+expect.extend({ matchers });
+
+afterEach(() => {
+	cleanup();
+});
