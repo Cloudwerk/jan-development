22cbd439caf70660e077e0b84dca92c11a2500e2
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-02-09 12:46:46 +0100

    React Adv. 50: refactor Toast Component

--- React_Adv/50-toast/src/Toast.tsx
+++ React_Adv/50-toast/src/Toast.tsx
@@ -1,20 +1,8 @@
-import { toastPositions } from "./utils/models";
-
 type ToastProps = {
-	position: toastPositions;
-	messages: Array<string>;
+	message: string;
+	id: number;
 };
 
-export function Toast({ position, messages }: ToastProps) {
-	return (
-		<div className={`toast-container ${position}`}>
-			{messages.map((message, index) => {
-				return (
-					<div className="toast" data-id={index}>
-						{message}
-					</div>
-				);
-			})}
-		</div>
-	);
+export function Toast({ message, id }: ToastProps) {
+	return <>{<div className="toast">{message}</div>}</>;
 }
