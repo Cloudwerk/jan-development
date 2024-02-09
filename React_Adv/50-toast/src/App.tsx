f2f65e5c0c62669a34b7cfe0da2cfd48c3138d13
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-02-09 12:47:03 +0100

    React Adv. 50: Add Toast Containe to App

--- React_Adv/50-toast/src/App.tsx
+++ React_Adv/50-toast/src/App.tsx
@@ -1,11 +1,14 @@
 import "./styles.css";
+import { ToastContextProvider } from "./ToastContextProvider";
 import { ToastForms } from "./ToastForms";
+import { ToastsContainer } from "./ToastsContainer";
 
 function App() {
 	return (
-		<>
+		<ToastContextProvider>
 			<ToastForms />
-		</>
+			<ToastsContainer />
+		</ToastContextProvider>
 	);
 }
 
