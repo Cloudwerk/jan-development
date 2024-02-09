6b681957d7c98838b9c33d25e5ba866d56f0ae42
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-02-09 13:21:04 +0100

    React Adv. 50: toasts destroy themselves on click

--- React_Adv/50-toast/src/Toast.tsx
+++ React_Adv/50-toast/src/Toast.tsx
@@ -1,8 +1,26 @@
+import { useToastDispatchContext } from "./ToastContextProvider";
+import { toastPositions } from "./utils/models";
+
 type ToastProps = {
 	message: string;
 	id: number;
+	position: toastPositions;
+	autoDismiss: boolean;
 };
 
-export function Toast({ message, id }: ToastProps) {
-	return <>{<div className="toast">{message}</div>}</>;
+export function Toast({ message, id, position, autoDismiss }: ToastProps) {
+	const dispatchToasts = useToastDispatchContext();
+
+	function deleteSelf() {
+		dispatchToasts({ action: "DEL", payload: { message, id, position, autoDismiss } });
+	}
+	return (
+		<>
+			{
+				<div className="toast" onClick={() => deleteSelf()}>
+					{message}
+				</div>
+			}
+		</>
+	);
 }
