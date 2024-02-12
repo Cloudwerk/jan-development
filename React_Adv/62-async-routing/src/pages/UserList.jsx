192c2fc741351badddf5cc8569ef22e2d3269a74
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-02-12 11:33:35 +0100

    React-Adv. 62: add Loading Animation to users Page

--- React_Adv/62-async-routing/src/pages/UserList.jsx
+++ React_Adv/62-async-routing/src/pages/UserList.jsx
@@ -1,38 +1,46 @@
-import { Link, useLoaderData } from "react-router-dom"
-import { getUsers } from "../api/users"
+import { Await, Link, defer, useLoaderData } from "react-router-dom";
+import { getUsers } from "../api/users";
+import { Suspense } from "react";
+import { CardsFallback } from "../components/SkeletonCard";
 
 function UserList() {
-  const users = useLoaderData()
+	const { data: users } = useLoaderData();
 
-  return (
-    <>
-      <h1 className="page-title">Users</h1>
-      <div className="card-grid">
-        {users.map(user => (
-          <div key={user.id} className="card">
-            <div className="card-header">{user.name}</div>
-            <div className="card-body">
-              <div>{user.company.name}</div>
-              <div>{user.website}</div>
-              <div>{user.email}</div>
-            </div>
-            <div className="card-footer">
-              <Link className="btn" to={user.id.toString()}>
-                View
-              </Link>
-            </div>
-          </div>
-        ))}
-      </div>
-    </>
-  )
+	return (
+		<>
+			<h1 className="page-title">Users</h1>
+			<div className="card-grid">
+				<Suspense fallback={<CardsFallback />}>
+					<Await resolve={users}>
+						{(users) =>
+							users.map((user) => (
+								<div key={user.id} className="card">
+									<div className="card-header">{user.name}</div>
+									<div className="card-body">
+										<div>{user.company.name}</div>
+										<div>{user.website}</div>
+										<div>{user.email}</div>
+									</div>
+									<div className="card-footer">
+										<Link className="btn" to={user.id.toString()}>
+											View
+										</Link>
+									</div>
+								</div>
+							))
+						}
+					</Await>
+				</Suspense>
+			</div>
+		</>
+	);
 }
 
 function loader({ request: { signal } }) {
-  return getUsers({ signal })
+	return defer({ data: getUsers({ signal }) });
 }
 
 export const userListRoute = {
-  loader,
-  element: <UserList />,
-}
+	loader,
+	element: <UserList />,
+};
