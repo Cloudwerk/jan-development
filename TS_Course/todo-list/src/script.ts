bc64614c6b70f1d95151d7811924e3055640b0f1
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-02-26 16:35:59 +0100

    ts-todo: create new list items through form

--- TS_Course/todo-list/src/script.ts
+++ TS_Course/todo-list/src/script.ts
@@ -1,18 +1,40 @@
 const form = document.querySelector<HTMLFormElement>("#new-todo-form");
 const input = document.querySelector<HTMLInputElement>("#todo-input");
 const list = document.querySelector<HTMLUListElement>("#list");
-const template = document.querySelector<HTMLTemplateElement>("#todo-template");
 
 setupEventListeners();
 
 function setupEventListeners() {
-  if (!form || !input || !list || !template) {
+  if (!form || !input || !list) {
     alert("error while quering the DOM elements");
     return;
   }
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    alert("here");
+    if (input.value === "") return;
+    const listItem = document.createElement("li");
+    listItem.classList.add("list-item");
+
+    const label = document.createElement("label");
+    label.classList.add("list-item-label");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("label-input");
+
+    const span = document.createElement("span");
+    span.textContent = input.value;
+
+    const button = document.createElement("button");
+    button.classList.add("delete-btn");
+    button.textContent = "Delete";
+    button.addEventListener("click", () => {
+      listItem.remove();
+    });
+
+    label.append(checkbox, span);
+    listItem.append(label, button);
+    list.append(listItem);
   });
 }
